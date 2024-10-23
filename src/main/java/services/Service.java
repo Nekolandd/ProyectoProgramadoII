247879f33b4ccb8af3e08eb2e4a586a54187a02e
Diana <dianamonterrey04@gmail.com>
2024-10-23 11:02:18 -0600

    InnitComit

---
+++ src/main/java/services/Service.java
@@ -0,0 +1,13 @@
+package services;
+
+import java.util.List;
+
+public abstract class Service<T>  {
+    public abstract List<T> getAll() throws Exception;
+    public abstract T getById(int id) throws Exception;
+    public abstract void store(T pojo) throws Exception;
+    public abstract void delete(Integer id) throws Exception;
+    public abstract void update(T pojo) throws Exception;
+
+
+}
